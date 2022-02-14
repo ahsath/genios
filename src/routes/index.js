c1ff1cb74d8b7ef8f71883673c2ce2faccd59e04
Ahsath <ahsath@gmail.com>
2022-02-14 21:19:46 +0000

    refactor(index): update endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { stringify } from 'qs';
+import { currency, createAdaptiveMediaSource } from '~/helpers';
 
 export async function get() {
   const query = stringify(
@@ -15,19 +16,28 @@ export async function get() {
   );
 
   try {
-    const { data } = await (
-      await fetch(`https://stage.genioreis.com/api/properties?${query}`)
-    ).json();
+    const res = await fetch(`${process.env.API_BASE_URL}/properties?${query}`);
+    const { data } = await res.json();
+
+    const properties = data.map(({ id, attributes }) => {
+      const { image, price, ...rest } = attributes;
+      return {
+        id,
+        price: currency.format(price),
+        image: createAdaptiveMediaSource(image.data.attributes.formats),
+        ...rest,
+      };
+    });
 
     return {
       body: {
-        properties: data,
+        properties,
       },
     };
   } catch (error) {
-    // console.log(error);
     return {
       status: 500,
+      body: { error },
     };
   }
 }
