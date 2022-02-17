28f0a473bff28fdfd4a1c66ef0bfef1043b3c168
Ahsath <ahsath@gmail.com>
2022-02-17 01:34:08 +0000

    feat(index.svelte): enable metatags

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,6 @@
-<!-- <Metatags {metatags}/> -->
+<Metatags {metatags}/>
 
 <Container class="grid gap-4 sm:gap-6">
-  <!-- <h1 class="[font-size:clamp(1.5rem,2.5vw,2.25rem)] tracking-wide leading-tight font-bold uppercase pt-20">Genios</h1>
-  <div class="font-['Scheherazade_New'] text-sm tracking-widest uppercase">Colares</div> -->
   {#each properties as property, i}
     <PropertyCard property={property} class={(i + 1) % 2 === 0 && 'sm:order-1'}/>
   {/each}
