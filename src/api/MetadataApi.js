9a965541e5a08a2a78a1fa95764a3d585728c534
Ahsath <ahsath@gmail.com>
2022-02-17 19:15:23 +0000

    feat(MetadataApi): add `fetchFooter` api method

--- src/api/MetadataApi.js
+++ src/api/MetadataApi.js
@@ -4,6 +4,10 @@ class MetadataApi extends Api {
   fetchMetadata(path, config) {
     return fetch(`${this.BASE_URL}${path}?${config.query}`);
   }
+
+  fetchFooter(path, config) {
+    return fetch(`${this.BASE_URL}${path}?${config.query}`);
+  }
 }
 
 export default new MetadataApi();
