2062efffa291cce5d9a9d7ee26e70f6dffe038bb
Ahsath <ahsath@gmail.com>
2022-02-17 19:21:19 +0000

    refactor(Contact.svelte): add social contact from footer context

--- src/components/footer/Contact.svelte
+++ src/components/footer/Contact.svelte
@@ -1,8 +1,14 @@
 <div class="flex items-center flex-col gap-4">
-  <span class="font-['Scheherazade_New'] text-sm tracking-widest uppercase">Contact</span>
+  <span class="font-['Scheherazade_New'] text-sm tracking-widest uppercase">{footer.data.contactDisplayText}</span>
   <div class="flex gap-6 tracking-widest font-bold uppercase">
-    <a href="https://wa.me/351911013350" target="_blank">Whatsapp</a>
-    <a href="mailto:ahsath@gmail.com" target="_blank">Email</a>
-    <a href="tel:+351911013350">Phone</a>
+    {#each footer.data.networks as network}
+      <a href={network.link} target="_blank">{network.name}</a>
+    {/each}
   </div>
 </div>
+
+<script>
+import { getContext } from 'svelte'
+
+let footer = getContext('footer')
+</script>
