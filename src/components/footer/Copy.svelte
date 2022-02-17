0c05c8e847a2477bb76d9cf6162cfa7e9e5a88a5
Ahsath <ahsath@gmail.com>
2022-02-17 19:20:47 +0000

    refactor(Copy.svelte): add copy from footer context

--- src/components/footer/Copy.svelte
+++ src/components/footer/Copy.svelte
@@ -1,3 +1,7 @@
-<small
-class="text-center text-black/60"
->Copyright © { new Date().getFullYear() } Génios Cosmopolitas, LDA. NIF 513 044 612 - 2705-628 São João das Lampas, Portugal</small>
+<small class="text-center text-black/60">
+  {getContext('footer').data.copy}
+</small>
+
+<script>
+import { getContext } from 'svelte'
+</script>
